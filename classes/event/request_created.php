8d0d55a3c11e4baca790d2fd7ebe0d06196f0ee2
Nicholas Hoobin <nicholashoobin@catalyst-au.net>
2016-07-26 11:20:20 +1000

    Request creation event.

---
+++ classes/event/request_created.php
@@ -0,0 +1,43 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Request creation event.
+ *
+ * @package    local_extension
+ * @author     Nicholas Hoobin <nicholashoobin@catalyst-au.net>
+ * @copyright  Catalyst IT
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace local_extension\event;
+
+defined('MOODLE_INTERNAL') || die();
+
+class request_created extends \core\event\base {
+    protected function init() {
+        $this->data['crud'] = 'c';
+        $this->data['edulevel'] = self::LEVEL_PARTICIPATING;
+    }
+
+    public static function get_name() {
+        return get_string('event_request_created', 'local_extension');
+    }
+
+    public function get_description() {
+        return "A request has been created";
+    }
+}
