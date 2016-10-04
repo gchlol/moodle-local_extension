050bebea0b91051990a51d7e7a18c0e51267e2d1
Nicholas Hoobin <nicholashoobin@catalyst-au.net>
2016-10-05 09:46:08 +1100

    Additional access checking.

--- modify.php
+++ modify.php
@@ -44,20 +44,23 @@ $request = utility::cache_get_request($requestid);
 // The list of subscribed users populated each time the request object is generated.
 // The request object is invalidated and regenerated after each comment, attachment added, or rule triggered.
 
+// Checking if the current user is not part of the request.
 if (!array_key_exists($USER->id, $request->users)) {
-
-    // Admin users will have this capability, or anyone that was subscribed.
     $context = context_module::instance($cmid);
+    // Admin users will have this capability, or anyone that was subscribed.
     if (!has_capability('local/extension:viewallrequests', $context)) {
+        die();
+    }
+
+} else {
+    // The user is part of the request, lets check their access.
+    $access = $request->get_user_access($USER->id, $request->cms[$cmid]->cm->id);
+    if ($access != \local_extension\rule::RULE_ACTION_APPROVE &&
+        $access != \local_extension\rule::RULE_ACTION_FORCEAPPROVE) {
         die();
     }
 }
 
-$access = $request->get_user_access($USER->id, $request->cms[$cmid]->cm->id);
-if ($access != \local_extension\rule::RULE_ACTION_APPROVE &&
-    $access != \local_extension\rule::RULE_ACTION_FORCEAPPROVE) {
-    die();
-}
 
 $url = new moodle_url('/local/extension/modify.php', array('id' => $requestid, 'course' => $courseid, 'cmid' => $cmid));
 $PAGE->set_url($url);
