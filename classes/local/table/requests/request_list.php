023174a7a155d62a1e32995ca3d3ce61a47156df
Nicholas Hoobin <nicholashoobin@catalyst-au.net>
2017-05-16 13:03:46 +1000

    Add require to tablelib in request_list.php

--- classes/local/table/requests/request_list.php
+++ classes/local/table/requests/request_list.php
@@ -29,6 +29,8 @@ if (!defined('MOODLE_INTERNAL')) {
     die('Direct access to this script is forbidden.'); // It must be included from a Moodle page.
 }
 
+require_once($CFG->libdir.'/tablelib.php');
+
 use html_writer;
 use local_extension\state;
 use local_extension\utility;
