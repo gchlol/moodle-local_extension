2120bc8a22cd77849bcd7d5b0d549bdbd4eeb39b
Nicholas Hoobin <nicholashoobin@catalyst-au.net>
2017-06-07 16:17:08 +1000

    Revert "Issue #52, email immediate request updates with the configured username."
    This reverts commit c50b1a206ef4cc833d65e79693772b56aa194d88.
    
     Conflicts:
    	classes/request.php

--- classes/request.php
+++ classes/request.php
@@ -626,6 +626,12 @@ class request implements \cache_data_source {
                 }
             }
 
+
+
+            // The update is sent from who modified the history.
+            // There will always be at least one history item.
+            $userfrom = core_user::get_user($history[0]->userid);
+
             $requestuser = core_user::get_user($this->request->userid);
             $fullname = \fullname($requestuser, true);
 
@@ -645,18 +651,8 @@ class request implements \cache_data_source {
 
             $content = get_string('notification_footer', 'local_extension', $obj);
 
-            // Setup the noreply user name.
             $noreplyuser = core_user::get_noreply_user();
-            $supportusername = get_config('local_extension', 'supportusername');
-            if (!empty($supportusername)) {
-                // If the plugin has the support username configured, use that name.
-                $noreplyuser->firstname = $supportusername;
-            } else {
-                // The update is sent from who modified the history.
-                // There will always be at least one history item.
-                $userfrom = core_user::get_user($history[0]->userid);
-                $noreplyuser->firstname = \fullname($userfrom, true);
-            }
+            $noreplyuser->firstname = \fullname($userfrom, true);
 
             utility::send_trigger_email($this, $subject, $content, $noreplyuser, $userto);
         }
