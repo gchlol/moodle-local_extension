dd32ffadd4385862e7f0cbcf0dbdba7846f11fc2
Kristian Ringer <kristian.ringer@catalyst-au.net>
2019-01-15 15:10:26 +1100

    Replace deprecated function pix_url with pix_icon

--- classes/local/table/rules.php
+++ classes/local/table/rules.php
@@ -130,7 +130,7 @@ class rules extends flexible_table {
             ));
 
             $edithtml = html_writer::empty_tag('img', array(
-                'src' => $renderer->pix_url('t/edit'),
+                'src' => $renderer->pix_icon('t/edit', ''),
                 'alt' => get_string('edit'),
                 'class' => 'iconsmall'
             ));
@@ -141,7 +141,7 @@ class rules extends flexible_table {
             ));
 
             $deletehtml = html_writer::empty_tag('img', array(
-                'src' => $renderer->pix_url('t/delete'),
+                'src' => $renderer->pix_icon('t/delete', ''),
                 'alt' => get_string('delete'),
                 'class' => 'iconsmall'
             ));
